f0d98430cfc7bb7ca0261e8c5f477ade9c55158e
Mathieu Comandon <strycore@gmail.com>
2019-02-06 18:41:40 -0800

    Create project

---
+++ src/views/dashboard/admin/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div/>
+</template>
+
+<script>
+export default {
+  name: 'DashboardAdmin',
+  components: {},
+  data() {
+    return {}
+  },
+  methods: {}
+}
+</script>
+
+<style rel="stylesheet/scss" lang="scss" scoped>
+.dashboard-editor-container {
+  padding: 32px;
+  background-color: rgb(240, 242, 245);
+  .chart-wrapper {
+    background: #fff;
+    padding: 16px 16px 0;
+    margin-bottom: 32px;
+  }
+}
+</style>
---
+++ src/views/dashboard/editor/index.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="dashboard-editor-container">
+    <div class="clearfix">
+      <div class="info-container">
+        <span class="display_name">{{ name }}</span>
+        <span>Moderation Dashboard</span>
+      </div>
+    </div>
+    <div>
+      <span>Content here</span>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+
+export default {
+  name: 'DashboardEditor',
+  components: { },
+  data() {
+    return {}
+  },
+  computed: {
+    ...mapGetters([
+      'name',
+      'avatar',
+      'roles'
+    ])
+  }
+}
+</script>
+
+<style rel="stylesheet/scss" lang="scss" scoped>
+  .dashboard-editor-container {
+    background-color: #e3e3e3;
+    min-height: 100vh;
+    padding: 50px 60px 0px;
+
+    .info-container {
+      .display_name {
+        font-size: 2em;
+        color: #212121;
+      }
+    }
+  }
+</style>
