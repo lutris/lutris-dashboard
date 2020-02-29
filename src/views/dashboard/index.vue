352d140cb61934010eb6561537d9956daa132bc5
Mathieu Comandon <strycore@gmail.com>
2020-02-28 20:20:33 -0800

    Display stats on dashboard home

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,25 +1,59 @@
 <template>
   <div class="dashboard-container">
-    <!-- <component :is="currentRole"/> -->
+    <div class="info-container">
+      <p>Games: {{ stats.games }}</p>
+      <p>Unpublished games: {{ stats.unpublished_games }}</p>
+      <p>Installers : {{ stats.installers }}</p>
+      <p>Unpublished installers: {{ stats.unpublished_installers }}</p>
+      <p>Screenshots : {{ stats.screenshots }}</p>
+      <p>Unpublished screenshots: {{ stats.unpublished_screenshots }}</p>
+    </div>
   </div>
 </template>
 
 <script>
-// import { mapGetters } from 'vuex'
-import adminDashboard from './admin'
-import editorDashboard from './editor'
-
+import { fetchStats } from '@/api/installers'
 export default {
   name: 'Dashboard',
-  components: { adminDashboard, editorDashboard },
+  components: {},
   data() {
     return {
-      currentRole: 'adminDashboard'
+      statsLoading: false,
+      stats: {
+        'games': 0,
+        'unpublished_games': 0,
+        'published_games': 0,
+        'installers': 0,
+        'unpublished_installers': 0,
+        'screenshots': 0,
+        'unpublished_screenshots': 0
+      }
     }
   },
   computed: {},
   created() {
-    this.currentRole = 'editorDashboard'
+    this.getStats()
+  },
+  methods: {
+    getStats() {
+      this.statsLoading = true
+      fetchStats().then(response => {
+        this.stats = response.data
+        console.log(this.stats)
+        this.statsLoading = false
+      })
+    }
   }
 }
 </script>
+
+<style rel="stylesheet/scss" lang="scss" scoped>
+  .dashboard-container {
+    padding: 50px 60px 0px;
+
+    .info-container {
+      font-size: 2em;
+      color: #414141;
+    }
+  }
+</style>
