1b6f501b2bf1cf5b74089826a091f311fe0b93cf
Mathieu Comandon <strycore@gmail.com>
2023-01-05 05:38:06 -0800

    More detailed stats

--- src/views/main/dashboard/index.vue
+++ src/views/main/dashboard/index.vue
@@ -2,12 +2,19 @@
   <div class="box">
     <div class="info-container">
       <p>Games: {{ stats.games }}</p>
-      <p>Submissions: <a href="/#/games/game_submissions">{{ stats.game_submissions }}</a></p>
+      <p>Published games: {{ stats.published_games }}</p>
       <p>Unpublished games: {{ stats.unpublished_games }}</p>
+      <p>Submissions: <a href="/#/games/game_submissions">{{ stats.game_submissions }}</a></p>
+      <hr/>
+
       <p>Installers : {{ stats.installers }}</p>
+      <p>Published installers: {{ stats.published_installers }}</p>
       <p>Unpublished installers: {{ stats.unpublished_installers }}</p>
       <p>Draft installers: {{ stats.drafts }}</p>
+      <hr/>
+
       <p>Screenshots : {{ stats.screenshots }}</p>
+      <p>Published screenshots: {{ stats.published_screenshots }}</p>
       <p>Unpublished screenshots: {{ stats.unpublished_screenshots }}</p>
     </div>
   </div>
@@ -24,13 +31,15 @@ export default {
       statsLoading: false,
       stats: {
         'games': 0,
-        'game_submissions': 0,
-        'unpublished_games': 0,
         'published_games': 0,
+        'unpublished_games': 0,
+        'game_submissions': 0,
         'installers': 0,
+        'published_installers': 0,
         'unpublished_installers': 0,
         'drafts': 0,
         'screenshots': 0,
+        'published_screenshots': 0,
         'unpublished_screenshots': 0
       }
     }
