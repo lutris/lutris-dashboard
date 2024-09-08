ae30d155602e93e8133e4820331b0ea96fa2591e
Mathieu Comandon <strycore@gmail.com>
2024-09-08 16:30:34 -0700

    Use correct domain name for changes url

--- src/views/games/gameChangeTable.vue
+++ src/views/games/gameChangeTable.vue
@@ -1,45 +1,44 @@
 <template>
-    <el-table
-      v-loading="loading"
-      key="id"
-      :data="changes"
-      fit
-      highlight-current-row>
-      <el-table-column label="Name" prop="game">
-        <template #default="props">
-          <a :href="'https://lutris.net/games/' + props.row['game'].change_for.slug">{{ props.row["game"].name }}</a>
-        </template>
-      </el-table-column>
-      <el-table-column label="User" prop="user">
-        <template #default="props">
-          {{ props.row["user"].username }}<br>
-          {{ props.row["created_at"] }}<br>
-        </template>
-      </el-table-column>
-    </el-table>
-  </template>
+  <el-table v-loading="loading" key="id" :data="changes" fit highlight-current-row>
+    <el-table-column label="Name" prop="game">
+      <template #default="props">
+        <a :href="baseUrl + '/games/' + props.row['game'].change_for.slug">{{ props.row["game"].name }}</a>
+      </template>
+    </el-table-column>
+    <el-table-column label="User" prop="user">
+      <template #default="props">
+        {{ props.row["user"].username }}<br>
+        {{ props.row["created_at"] }}<br>
+      </template>
+    </el-table-column>
+  </el-table>
+</template>
 
-  <script>
-  export default {
-    name: 'GameChangeTable',
-    props: {
-      loading: {
-        type: Boolean,
-        default: false
-      },
-      changes: {
-        type: Array,
-        default: () => []
-      }
+<script>
+export default {
+  name: 'GameChangeTable',
+  props: {
+    loading: {
+      type: Boolean,
+      default: false
     },
-    methods: {
-      getChangeIndex(changeId) {
-        for (let index = 0; index < this.changes.length; index++) {
-          if (this.changes[index].id === changeId) {
-            return index
-          }
+    changes: {
+      type: Array,
+      default: () => []
+    },
+    baseUrl: {
+      type: String,
+      default: import.meta.env.VITE_BASE_URL,
+    }
+  },
+  methods: {
+    getChangeIndex(changeId) {
+      for (let index = 0; index < this.changes.length; index++) {
+        if (this.changes[index].id === changeId) {
+          return index
         }
       }
     }
   }
-  </script>
+}
+</script>
